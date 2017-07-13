27989b9f978aa7c5d0ebd41619117053e20e2f0c
LevoirE <Liva_E@outlook.com>
2017-07-13 19:33:56 +0800

    Delete FreeRTOS Demo Floder

--- FreeRTOS/Source/include/FreeRTOSConfig.h
+++ FreeRTOS/Source/include/FreeRTOSConfig.h
@@ -115,7 +115,7 @@ to exclude the API function. */
 #define configKERNEL_INTERRUPT_PRIORITY 		255
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	191 /* equivalent to 0xb0, or priority 11. */
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	0x1F /* equivalent to 0xb0, or priority 11. */
 
 
 /* This is the value being used as per the ST library which permits 16
@@ -123,6 +123,8 @@ priority values, 0 to 15.  This must correspond to the
 configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
 NVIC value of 255. */
 #define configLIBRARY_KERNEL_INTERRUPT_PRIORITY	15
+
+#define configASSERT(x) if((x) == 0) { taskDISABLE_INTERRUPTS(); for(;;); }
 
 #endif /* FREERTOS_CONFIG_H */
 
